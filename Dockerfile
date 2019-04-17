45ead0337f393534fe2821250d60d4e66be8dacd
NEXUS <luckas.ribeiro2015@gmail.com>
2019-04-17 16:03:31 -0300

    portas trocadas

--- Dockerfile
+++ Dockerfile
@@ -14,6 +14,6 @@ COPY --chown=node:node . .
 
 USER node
 
-EXPOSE 8500
+EXPOSE 8501
 
 CMD [ "node", "app.js" ]
--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const port=process.env.PORT || 8501
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -37,5 +39,10 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(port, ()=> {
+
+  console.log(`Server running at port `+port);
+  });
 
 module.exports = app;
